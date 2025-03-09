8acfe6295d11ab5f406b5256de4bf99525fd20b1
stagib <147700418+stagib@users.noreply.github.com>
2025-03-09 21:37:23 +1100

    add chat template

---
+++ src/lib/components/Message.svelte
@@ -0,0 +1,9 @@
+<script>
+	let { username, content } = $props();
+</script>
+
+<div class="px-4 py-1 hover:bg-neutral-800">
+	<span class="text-sm text-neutral-400">00:00 PM</span>
+	<span class="text-sm text-amber-300">{username}</span><span>:</span>
+	<span class="text-sm">{content}</span>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Message from '$lib/components/Message.svelte';
+	import MessageInput from '$lib/components/MessageInput.svelte';
+</script>
+
+<div class="flex h-screen max-h-screen justify-center py-8">
+	<div
+		class="flex h-full max-h-screen w-full max-w-sm flex-col rounded-sm border border-neutral-800 bg-neutral-950"
+	>
+		<div class="flex items-center justify-center border-b border-neutral-800 py-2">
+			<div class="text-sm font-semibold">LIVE CHAT</div>
+		</div>
+
+		<div class="flex h-full flex-col overflow-y-auto">
+			{#each { length: 30 } as _, i}
+				<Message username={'username'} content={'dsLLSDSMLD LKSMDKLMSDKML MASDLKMASDKLM MSAMMDM'} />
+				<Message username={'username'} content={'liftstule'} />
+				<Message username={'username'} content={'ta'} />
+				<Message username={'username'} content={'nah'} />
+				<Message
+					username={'username'}
+					content={'raedy lkie sme itne msire windos smk mmm kkk123'}
+				/>
+			{/each}
+		</div>
+
+		<MessageInput />
+	</div>
+</div>
